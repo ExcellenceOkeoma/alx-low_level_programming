b5b29e7ff0a4d24beaa5a7217ba1e28da62c5b5f
ExcellenceOkeoma <ExcellenceOkeoma@gmail.com>
2023-04-25 07:04:08 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  Main -Entry point
+ *
+ *  Return: always 0 (Success)
+ */
+init main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
