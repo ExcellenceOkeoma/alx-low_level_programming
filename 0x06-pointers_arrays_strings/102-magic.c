4ba5e49cb529d476077e04eb88e808ae05992219
ExcellenceOkeoma <ExcellenceOkeoma@gmail.com>
2023-05-04 10:41:28 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,8 @@ int main(void)
   a[2] = 1024;
   p = &n;
   /*
-   * - line fof code at  edpedted line
+   * write your line of code here...
+ 
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
