11b6894b4df5dc74014e4f7e738b9eabfec3f236
ExcellenceOkeoma <ExcellenceOkeoma@gmail.com>
2023-06-01 14:51:23 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,6 +19,12 @@ int _strlen(const char *s)
 	return (i);
 }
 
+/**
+ * add_node - add a new node at beginning of a list_t list.
+ * @head: head of a list_t list.
+ * @str: value to insert into element.
+ * Return: the number of nodes.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *add;
