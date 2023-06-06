d38e8026b1fa6862f3fe1eab7bc23e55c8a443f2
ExcellenceOkeoma <ExcellenceOkeoma@gmail.com>
2023-06-06 07:31:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints all the elements in a listint_t list.
+ * print_listint - prints all the elements of a listint_t list.
  *
- * @h: head of pointer
+ * @h: head of linklist node
  *
  * Return: the number of nodes
  *
